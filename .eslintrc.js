2eabb82577adc5e70f797cfb381fd60699bbace4
ww9 <ww9nine9@gmail.com>
2021-12-02 17:10:53 -0400

    disable eslinting of .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
 		'browser': true,
 		'es2021': true
 	},
+	'ignorePatterns': ['.eslintrc.js', '**/vendor/*.js'],
 	'extends': [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended'
